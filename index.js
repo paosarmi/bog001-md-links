8626f2163f2c11d5ce7f2b48f6876a65f4e99548
DayanaM <dmmartinez269@misena.edu.co>
2020-09-21 15:50:19 -0500

    Función lectura de archivos.md

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ const fs = require("fs");
 
 //Leer archivo.md
 
-fs.readFile("TEXT.md", { encoding: "utf8" }, function (error, data) {
+/*fs.readFile("TEXT.md", { encoding: "utf8" }, function (error, data) {
   if (error) {
     console.log("Error: ", error);
   } else {
     console.log("Datos leidos: ", data);
   }
-});
+});*/
+
+fs.readFile("TEXT.md", { encoding: "utf8" }, readFiles);
+function readFiles(error, data) {
+  if (error) {
+    console.log("Error: ", error);
+  } else {
+    console.log("Datos leidos: ", data);
+  }
+}
