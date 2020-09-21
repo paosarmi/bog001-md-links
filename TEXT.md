e3556cb74ef25171a862abcbbbb9ec79d7567e36
DayanaM <dmmartinez269@misena.edu.co>
2020-09-20 20:25:55 -0500

    Leer archivo.md

---
+++ TEXT.md
@@ -0,0 +1,5 @@
+# Markdown Links
+
+## El presente texto es una prueba-
+
+El desarrollo front-end es maravilloso.
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-module.exports = () => {
+/*module.exports = () => {
   // ...
-};
+};*/
+
+const fs = require("fs");
+
+//Leer archivo.md
+
+fs.readFile("TEXT.md", { encoding: "utf8" }, function (error, data) {
+  if (error) {
+    console.log("Error: ", error);
+  } else {
+    console.log("Datos leidos: ", data);
+  }
+});
