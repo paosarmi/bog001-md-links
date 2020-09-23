91bcdda2a78966d1f45396c38c4512684b5291d1
DayanaM <dmmartinez269@misena.edu.co>
2020-09-23 10:11:32 -0500

    Extraer links

--- TEXT.md
+++ TEXT.md
@@ -3,3 +3,5 @@
 ## El presente texto es una prueba-
 
 El desarrollo front-end es maravilloso.
+
+[Facebook](https://www.facebook.com) Es una red social
--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-/*module.exports = () => {
-  // ...
-};*/
-
+//const { rejects } = require("assert");
 const fs = require("fs");
+//const { resolve } = require("path");
+const md = require("markdown-it")({
+  html: true,
+  linkify: true,
+});
 
 //Leer archivo.md
-
-/*fs.readFile("TEXT.md", { encoding: "utf8" }, function (error, data) {
-  if (error) {
-    console.log("Error: ", error);
-  } else {
-    console.log("Datos leidos: ", data);
-  }
-});*/
 
 fs.readFile("TEXT.md", { encoding: "utf8" }, readFiles);
 function readFiles(error, data) {
   if (error) {
     console.log("Error: ", error);
   } else {
-    console.log("Datos leidos: ", data);
+    //console.log("Datos leidos: ", data);
+    let result = md.renderInline(data);
+    console.log(result);
   }
 }
+
+//Extraer links de archivos.md
